2deb3b5874002268a9ed434fd55133ae4bb098c3
Jerald Humes <humesj@aldenv142.allegheny.edu>
2019-04-30 13:16:46 -0400

    Added Comments

--- Input.java
+++ Input.java
@@ -1,23 +1,24 @@
+// Calls in java utility.
 import java.util.*;
-
+// Creates input class.
 class Input {
 private Output outputter;
 Input(Output outputs) {
     outputs = outputter;
 }
-
+// Calls in a scanner for user's name.
 public String getName() {
     Scanner scan = new Scanner(System.in);
     String name = scan.nextLine();
     return name;
 }
-
+// Calls in a scanner for user's money.
 public int getCash() {
     Scanner money = new Scanner(System.in);
     int cash = money.nextInt();
     return cash;
 }
-
+// Calls in a scanner for user's bet.
 public int getBet(int cash) {
     Scanner sc = new Scanner(System.in);
     int bet = sc.nextInt();
@@ -28,7 +29,7 @@ public int getBet(int cash) {
         }
     return bet;
 }
-
+// Calls in a scanner for any yes or no choice.
 public boolean choiceIsYes() {
     Scanner doubledown = new Scanner(System.in);
     String doubled = doubledown.nextLine();
@@ -38,11 +39,11 @@ public boolean choiceIsYes() {
     }
     return doubled.equals("yes");
 }
-
+// Reads in user's answer to the above question.
 public boolean isyesorno(String answer) {
     return (answer.equals("yes") || answer.equals("no"));
 }
-
+// Calls in a scanner for the hit or stand user.
 public boolean choiceisHit() {
     Scanner hitter = new Scanner(System.in);
     String hit = hitter.nextLine();
@@ -52,7 +53,7 @@ public boolean choiceisHit() {
     }
     return hit.equals("hit");
 }
-
+// Reads in user's answer to the above question.
 public boolean ishitorstand(String hit) {
     return (hit.equals("hit") || hit.equals("stand"));
 }
